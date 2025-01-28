bff39e0cca3d9c336347547678d36450a1292fc5
Hampus Löwing <hampus.lowing@outlook.com>
2025-01-28 20:28:41 +0100

    chore: complete part 2

--- Car.java
+++ Car.java
@@ -1,11 +1,14 @@
 import java.awt.*;
 
-public abstract class Car {
+public abstract class Car implements Movable {
     private int nrDoors; // Number of doors on the car
     private double enginePower; // Engine power of the car
     private double currentSpeed; // The current speed of the car
     private Color color; // Color of the car
     private String modelName; // The car model name
+    private double x; // X position
+    private double y; // Y position
+    private double direction; // Direction in degrees (0 is to the right, 90 is up, 180 is left, 270 is down)
 
     public Car(int nrDoors, double enginePower, Color color, String modelName) {
         this.nrDoors = nrDoors;
@@ -13,6 +16,9 @@ public abstract class Car {
         this.currentSpeed = 0; // Cars are stationary when created
         this.color = color;
         this.modelName = modelName;
+        this.x = 0;
+        this.y = 0;
+        this.direction = 0; // Initial direction is to the right
     }
 
     public int getNrDoors(){
@@ -58,5 +64,35 @@ public abstract class Car {
     // TODO fix this method according to lab pm
     public void brake(double amount){
         decrementSpeed(amount);
+    }
+
+    @Override
+    public void move() {
+        // Convert direction to radians because Math.cos and Math.sin take radians
+        double radians = Math.toRadians(direction);
+        x += Math.cos(radians) * currentSpeed;
+        y += Math.sin(radians) * currentSpeed;
+    }
+
+    @Override
+    public void turnLeft() {
+        direction = (direction + 90) % 360;
+    }
+
+    @Override
+    public void turnRight() {
+        direction = (direction - 90 + 360) % 360;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getDirection() {
+        return direction;
     }
 }
